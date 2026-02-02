85dc2c3f2108223b1239f23ece04ab3e7adc5647
Amardeep1811 <amardeepsharma95238@gmail.com>
2026-02-03 02:17:26 +0530

    first commit

---
+++ BubbleSort.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+class BubbleSort{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.print("Enter the elements of the array : ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = input.nextInt();
+        }
+
+        bubbleSort(arr , n);
+    }
+
+    static void bubbleSort(int[] arr ,int n){
+        int breakpoint = 0;
+        for(int i = n-1 ; i >= 0; i-- ){
+            for(int j = 0 ; j < n-1; j++){
+                if (arr[j+1] < arr[j]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+
+                    // System.out.println("1 time");
+                    breakpoint = 1;
+                }
+            }
+            
+            if(breakpoint == 0 ){
+                break;
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
