184b35ee8db5b4124dfdf0e0613b44b6b6b355bf
Amardeep1811 <amardeepsharma95238@gmail.com>
2026-02-03 02:19:12 +0530

    Using selection sort

---
+++ SelectionSort.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+class SelectionSort{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.print("Enter the elements of the array : ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = input.nextInt();
+        }
+
+        selectionSort(arr , n);
+    }
+
+    static void selectionSort(int[] arr ,int n){
+        for(int i = 0 ; i <= n-2 ; i++ ){
+            int min = i;
+            for(int j = i+1 ; j <= n-1; j++){
+                if (arr[j] < arr[min]) min = j;
+            }
+            // System.out.print("Min element : " + arr[min]);
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
