61a3ce02235c02ef8acd30b5194d5b501d188012
Amardeep1811 <amardeepsharma95238@gmail.com>
2026-02-04 06:36:30 +0530

    Insertion sort using recursion

---
+++ InsertionSortUsingRecursion.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+class InsertionSortUsingRecursion{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.print("Enter the elements of the array : ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = input.nextInt();
+        }
+
+        insertionSort(arr ,n);
+
+        for(int i = 0 ; i < n ; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void insertionSort(int[] arr ,int i){
+        if(i >= arr.length -1) return;
+
+        int j = i;
+
+        while(j > 0 && arr[j-1] > arr[j]){
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+
+        insertionSort(arr, i+1);
+
+    }
+}
